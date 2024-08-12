d3294cb87b45736d89a53b3ac04d784896b5dbe4
djacirramos <djacirramos@gmail.com>
2024-08-12 19:20:54 -0300

    Add files via upload

---
+++ PPython.py
@@ -0,0 +1,15 @@
+print("Helo, World!")
+
+#1.4 - OPERADORES ARITMÉTICOS
+print(40+2)
+print(43-1)
+print(6*7)
+print(84/2)
+print(6**2+6)
+
+#1.5 - VALORES E TIPOS
+
+print(type(2))
+print(type(42.0))
+print(type("Hello, World!"))
+print(type("42.0"))
